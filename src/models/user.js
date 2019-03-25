f4c064710928aad11fb43157fd23b9105e2af5ab
dknty11 <nguyenhoaivinh.1111@gmail.com>
2019-03-25 14:37:48 +0700

    first commit

---
+++ src/models/user.js
@@ -0,0 +1,119 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Task = require('./task')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number!')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password can not be "password"')
+            }
+        }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }],
+    avatar: {
+        type: Buffer
+    }
+}, {
+    timestamps: true
+})
+
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
+// instance method
+
+userSchema.methods.toJSON = function () {
+    const userObject = this.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+    delete userObject.avatar
+    return userObject
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const token = jwt.sign({ _id: this._id.toString() }, process.env.JWT_SECRET)
+
+    this.tokens = this.tokens.concat({ token })
+    await this.save()
+
+    return token
+}
+
+// model method
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Username or password is incorrect!')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    
+    if (!isMatch) {
+        throw new Error('Username or password is incorrect!')
+    }
+
+    return user
+}
+
+// Hash password
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8)
+    }
+
+    next()
+})
+
+// Delete user task when user is removed
+userSchema.pre('remove', async function (next) {
+    await Task.deleteMany({ owner: this._id })
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
