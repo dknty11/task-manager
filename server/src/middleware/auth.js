921a2023b288450704a10107e9696ddba4ad50b8
vinh nguyen <nguyenhoaivinh.1111@gmail.com>
2019-05-27 17:21:41 +0700

    fixing get token for auth middleware

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -5,7 +5,7 @@ const jwtSecret = process.env.JWT_SECRET
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.cookies.token
+        const token = req.header('Authorization').replace('Bearer ', '')
         const decoded = jwt.verify(token, jwtSecret)
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
